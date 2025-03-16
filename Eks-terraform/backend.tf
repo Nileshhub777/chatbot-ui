b26b89ab54eb95d958715ee0ab8bc4cc75baf966
Nilesh M <nilesh496@gmail.com>
2025-03-16 15:46:14 +0530

    EKS

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "democloudopenui" # Replace with your actual S3 bucket name
+    bucket = "demoCloudopenui" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
